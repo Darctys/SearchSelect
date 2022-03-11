c6f0fa417c0b9bb79087dc0ee5af45653fda88ef
Darctys <yltimatym2002@maiil.ru>
2022-03-11 08:39:29 +0500

    правки

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SearchSelectComponent } from './search-select/search-select.component';
+import {FormsModule} from "@angular/forms";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import { SearchPipe } from './search-select/search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchSelectComponent,
+    SearchPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search-select/search-select.component.ts
+++ src/app/search-select/search-select.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchSelectComponent implements OnInit {
 
+  selectedValue: any;
+  searchTxt: any;
+
+  items = [{
+    value : 100,
+    viewValue : 100
+  },{
+    value : 200,
+    viewValue : 200
+  },{
+    value : 300,
+    viewValue : 300
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public value (){
+    console.log(this.selectedValue);
+  }
 }
